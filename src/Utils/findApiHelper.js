e8d96fb3b2f7f7de4581df47f15af6675a29a5f3
Amit Choudhary <Amit.Choudhary@Honeywell.com>
2021-01-20 13:58:54 +0530

    Update graph transformation logic

--- src/Utils/findApiHelper.js
+++ src/Utils/findApiHelper.js
@@ -2,6 +2,10 @@ const url = "https://dev-cbld-sas-findapi.dev.forge.connected.honeywell.com/v1/s
 const ApiKey = 'HdsXlhBoGZgsifFSOI0mGCCGB4qLw0oxNYxtiZeO' // don't push code with this detail
 const customerId = 'idf0a29149-660a-4493-bba1-f3ffa4388078' // don't push code with this detail
 
+const chillerSvg = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaHBDfGuds05HBovlOJ9hXKnlW9L7aWrq0fw&usqp=CAU"
+const chiller2 = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlIyENHpT0t4VMNGk9xjQMs86ylk2xuCEyeQ&usqp=CAU"
+const ahu1 = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjmk54qjAUrbrtokjet2HXyulUhvO-mKIqdQ&usqp=CAU"
+
 export const getNodes = (data, callBack) => {
 
     const config = {
